b5b7cbb80ca98427281cd2cd3863f2d1ed60f806
xingrong123 <teyxingrong@gmail.com>
2021-07-25 12:40:46 +0800

    improve accuracy of identifying the index each number belong to

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import cv2
-from numpy.core.arrayprint import IntegerFormat
 import pytesseract
 import numpy as np
 from os import listdir
@@ -11,24 +10,32 @@ pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesse
 conf = r'--oem 3 --psm 6 -c tessedit_char_whitelist=0123456789'
 
 
-def getIndex(hImg, wImg, x, y, w, h):
-    wSquare = wImg/9
-    hSquare = hImg/9
+def getIndex(hPuzzle, wPuzzle, hStart, wStart, x, y, w, h):
+    wSquare = wPuzzle/9
+    hSquare = hPuzzle/9
     if (w-x) > wSquare or (h-y) > hSquare:
-        raise ValueError("tesseract detect a number larger than one square")
-    horizontalIndex = int(x // wSquare) 
-    verticalIndex = int(8 - y // hSquare)
-    if horizontalIndex != w // wSquare or verticalIndex != 8 - h // hSquare:
-        raise ValueError("tesseract detect a number intersecting line")
+        raise ValueError("number larger than one square detected")
+    horizontalIndex = int((x - wStart) // wSquare)
+    verticalIndex = int(8 - (y - hStart) // hSquare)
+    # print(horizontalIndex)
+    # print("8 - ({y} - {h}) // {s} = {v}".format(y=y,h=hStart,s=hSquare,v=verticalIndex))
+    horizontalIndex2 = int((w - wStart) // wSquare)
+    verticalIndex2 = int(8 - (h - hStart) // hSquare)
+    # print(horizontalIndex2)
+    # print("8 - ({y} - {h}) // {s} = {v}".format(y=h,h=hStart,s=hSquare,v=verticalIndex2))
+    if horizontalIndex != horizontalIndex2 or verticalIndex != verticalIndex2:
+        raise ValueError("number intersecting line detected")
     return horizontalIndex + verticalIndex * 9
 
-def drawBox(checkedImage, originalImage, box, hImg, wImg, puzzleList):
+
+def drawBox(checkedImage, originalImage, box, hImg, wImg, hPuzzle, wPuzzle, hStart, wStart, puzzleList):
     box = box.split(' ')
     x, y, w, h = int(box[1]), int(box[2]), int(box[3]), int(box[4])
     # print("{x}, {y}, {w}, {h}".format(x=x, y=y, w=w, h=h))
-    index = getIndex(hImg, wImg, x, y, w, h)
+    # print("x = {x}".format(x = box[0]))
+    index = getIndex(hPuzzle, wPuzzle, hStart, wStart, x, y, w, h)
     if (puzzleList[index] != None):
-        raise ValueError('two numbers detected in the same index')
+        raise ValueError('two numbers in the same index detected')
     puzzleList[index] = box[0]
     cv2.rectangle(checkedImage, (x, hImg-y),
                   (w, hImg-h), (255, 255, 255), -1)
@@ -37,12 +44,8 @@ def drawBox(checkedImage, originalImage, box, hImg, wImg, puzzleList):
                 cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 255), 2)
 
 
-def readImage(name):
-
-    link = './images/' + name
-    original_image = cv2.imread(link)
-    processedImage = original_image.copy()
-    gray = cv2.cvtColor(processedImage, cv2.COLOR_BGR2GRAY)
+def processImage(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     thresh = cv2.threshold(
         gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
@@ -53,13 +56,14 @@ def readImage(name):
     cnts = cv2.findContours(
         detected_lines, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    # listOfHorizontalPoints = []
+    listOfHorizontalPoints = []
     for c in cnts:
         # print("c is {c}".format(c=c[0][0][0]))
-        # listOfHorizontalPoints.append(c[0][0][0])
-        cv2.drawContours(processedImage, [c], -1, (255, 255, 255), 2)
-    # wPuzzle = max(listOfHorizontalPoints) - min(listOfHorizontalPoints)
-    # print("w is {w}".format(w = wPuzzle))
+        listOfHorizontalPoints.append(c[0][0][0])
+        cv2.drawContours(image, [c], -1, (255, 255, 255), 2)
+    wStart = min(listOfHorizontalPoints)
+    wPuzzle = max(listOfHorizontalPoints) - wStart
+    print("w is {w}".format(w=wPuzzle))
 
     # removing horizontal lines from image
     horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 1))
@@ -68,19 +72,28 @@ def readImage(name):
     cnts = cv2.findContours(
         detected_lines, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    # listOfVerticalPoints = []
+    listOfVerticalPoints = []
     for c in cnts:
-        # print("c is {c}".format(c=c[0][0][1]))
-        # listOfVerticalPoints.append(c[0][0][1])
-        cv2.drawContours(processedImage, [c], -1, (255, 255, 255), 2)
-    # hPuzzle = max(listOfVerticalPoints) - min(listOfVerticalPoints)
-    # print("h is {w}".format(w = hPuzzle))
-    
+        # print("c2 is {c}".format(c=c[0][0][1]))
+        listOfVerticalPoints.append(c[0][0][1])
+        cv2.drawContours(image, [c], -1, (255, 255, 255), 2)
+    hStart = min(listOfVerticalPoints)
+    hPuzzle = max(listOfVerticalPoints) - hStart
+    print("h is {w}".format(w=hPuzzle))
+
     thresh = 175
     processedImage = cv2.threshold(
-        processedImage, thresh, 255, cv2.THRESH_BINARY)[1]
+        image, thresh, 255, cv2.THRESH_BINARY)[1]
     processedImage = cv2.cvtColor(processedImage, cv2.COLOR_BGR2RGB)
-    checked_image = processedImage.copy()
+    return processedImage, wStart, wPuzzle, hStart, hPuzzle
+
+
+def readImage(name):
+
+    link = './images/' + name
+    original_image = cv2.imread(link)
+
+    processedImage, wStart, wPuzzle, hStart, hPuzzle = processImage(original_image.copy())
     hImg, wImg = processedImage.shape[0], processedImage.shape[1]
 
     # print(pytesseract.image_to_string(image, config=conf))
@@ -90,27 +103,29 @@ def readImage(name):
 
     boxes = pytesseract.image_to_boxes(processedImage, config=conf)
     for box in boxes.splitlines():
-        drawBox(checked_image, original_image, box, hImg, wImg, puzzleList)
+        drawBox(processedImage, original_image, box, hImg, wImg, hPuzzle,
+                wPuzzle, hStart, wStart, puzzleList)
 
     # checked_image would be a full white image if all numbers are detected
     # below checks if checked_image is white
-    if np.mean(checked_image) <= 254.9:
+    if np.mean(processedImage) <= 254.9:
         for num in range(3):
-            newBoxes = pytesseract.image_to_boxes(checked_image, config=conf)
+            newBoxes = pytesseract.image_to_boxes(processedImage, config=conf)
             for newBox in newBoxes.splitlines():
-                drawBox(checked_image, original_image, newBox, hImg, wImg, puzzleList)
+                drawBox(processedImage, original_image,
+                        newBox, hImg, wImg, hPuzzle, wPuzzle, hStart, wStart, puzzleList)
 
-            if np.mean(checked_image) > 254.9:
+            if np.mean(processedImage) > 254.9:
                 break
-            checked_image = cv2.erode(checked_image, (3, 3), iterations=1)
+            processedImage = cv2.erode(processedImage, (3, 3), iterations=1)
 
-        if np.mean(checked_image) <= 254.9:
-            print(np.mean(checked_image))
+        if np.mean(processedImage) <= 254.9:
+            print(np.mean(processedImage))
             raise ValueError("puzzle not fully read")
-    
+
     print(puzzleList)
     cv2.imshow(name, original_image)
-    # cv2.imshow(name + " check", checked_image)
+    # cv2.imshow(name + " check", processedImage)
 
 
 def main():
